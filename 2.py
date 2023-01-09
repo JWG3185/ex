756a75f8caa419d95012fec6a83b114f38c0d766
JWG3185 <112459603+JWG3185@users.noreply.github.com>
2023-01-09 11:23:22 +0900

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-url = 'https://apis.data.go.kr/B552657/ErmctInfoInqireService/getEmrrmRltmUsefulSckbdInfoInqire?serviceKey=Zv%2FutM8zPV4Cpby0tQi%2FtiDmHPhc19Zr67HNB4XUKyLt89VHIxcmVtAdRSWES5ve3JQGe4P3OL6dyL6vChTt8Q%3D%3D&STAGE1=%EB%B6%80%EC%82%B0%EA%B4%91%EC%97%AD%EC%8B%9C&pageNo=1&numOfRows=100'
+url = 'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEmrrmRltmUsefulSckbdInfoInqire?serviceKey=Zv%2FutM8zPV4Cpby0tQi%2FtiDmHPhc19Zr67HNB4XUKyLt89VHIxcmVtAdRSWES5ve3JQGe4P3OL6dyL6vChTt8Q%3D%3D&STAGE1=%EB%B6%80%EC%82%B0%EA%B4%91%EC%97%AD%EC%8B%9C&pageNo=1&numOfRows=100'
 df1 = pd.read_xml(url, xpath='.//item')
 df1_c = df1.copy()
 df2 = df1_c[['dutyName','hpid','hvec','hvoc', 'dutyTel3']]
